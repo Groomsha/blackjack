f1a47f3d0634466461af5f841fb1bb71274014cc
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2022-06-08 14:04:26 +0300

    add: pygame wireframe files

--- game.py
+++ game.py
@@ -1,4 +1,28 @@
+from typing import Dict
+
+import pygame
+pygame.init()
+
 
 class Game:
-	def __init__(self) -> None:
-		pass
+	FPS: int = 30
+	WIDTH: int = 800
+	HEIGHT: int = 600
+
+	def __init__(self, settings: Dict) -> None:
+		pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+		pygame.display.set_icon(pygame.image.load('./images/blackjack.ico'))
+		pygame.display.set_caption(f'BlackJack ({settings.get("description")}) {settings.get("version_app")}')
+
+		self.clock = pygame.time.Clock()
+
+		self.main_game_loop()
+
+
+	def main_game_loop(self) -> None:
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					exit()
+
+			self.clock.tick(self.FPS)
--- mainapp.py
+++ mainapp.py
@@ -1,5 +1,15 @@
+from json import load
+from typing import Dict
+
 from game import Game
 
 
 if __name__ == '__main__':
-	start_game = Game()
+	try:
+		with open('settings.json', 'r') as file:
+			settings: Dict = load(file)
+	except FileNotFoundError as e:
+		print(e)
+	else:
+		settings.update({"version_app": "v.0.1"})
+		start_game = Game(settings)
